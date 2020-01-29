28321dd9f5e84799e281108e6c476d384731afd4
PaytonHill156 <hillpr@student.swosu.edu>
2020-01-29 11:46:42 -0600

    Did sorting problem for week two

---
+++ src/WeekTwo/ArraySort.java
@@ -0,0 +1,73 @@
+package WeekTwo;
+
+import java.util.concurrent.ThreadLocalRandom;
+import java.util.Arrays;
+import java.util.Collections;
+
+
+public class ArraySort {
+
+	//says hello to user
+		//input -- nothing
+		//return -- nothing
+	public void sayHello() {
+		System.out.println("Hello, today we're going to see how fast your number sorter is.");
+	}
+
+
+	//creates an array with the given size 
+		//input -- integer
+		//return -- array
+	public Integer[] sizeArray(int arraySize) {
+		Integer[] arrayToFill = new Integer [arraySize];
+
+		return arrayToFill;
+	}
+
+	//fills the array with random numbers. 
+		//input -- array
+		// return -- filled array
+		// I used this site for help https://stackoverflow.com/questions/363681/how-do-i-generate-random-integers-within-a-specific-range-in-java
+	public Integer[] fillArrayWithRandom(Integer[] arrayToFill) {
+
+		for (int i = 0; i < arrayToFill.length; i ++) {
+			int randomNum = ThreadLocalRandom.current().nextInt(-1000, 1000 + 1);
+			arrayToFill[i] = randomNum;
+		}
+
+		return arrayToFill;
+	}
+
+	//sorts array in reverse order
+		//input -- array
+		//output -- array
+	public Integer[] sortArrayDescending(Integer[] arrayToSort) {
+
+		Arrays.sort(arrayToSort, Collections.reverseOrder());
+
+		return arrayToSort;
+	}
+
+	//sorts array in ascending order
+		//input -- array
+		//return -- array
+	public Integer[] sortArrayAscending(Integer[] arrayToSort) {
+
+		Arrays.sort(arrayToSort);
+
+		return arrayToSort;
+	}
+
+	//prints array to console
+		//input -- array
+		//return -- nothing
+	public void printArray(Integer[] arrayToPrint) {
+
+		for (int i = 0; i < arrayToPrint.length; i++) {
+			if (i % 20 == 0 && i > 0) {
+				System.out.println();
+			}
+			System.out.print(arrayToPrint[i] + " ");
+		}
+	}
+}
---
+++ src/WeekTwo/ExecutionTime.java
@@ -0,0 +1,64 @@
+package WeekTwo;
+
+import java.util.Scanner;
+
+public class ExecutionTime {
+
+	public static void main(String[] args) {
+		/*(Execution time) Write a program that randomly generates 
+		 * an array of 100,000 integers and a key. Estimate the 
+		 * execution time of invoking the linearSearch method in 
+		 * Listing 7.6. Sort the array and estimate the execution 
+		 * time of invoking the binarySearch method in Listing 7.7. 
+		 * You can use the following code template to obtain the execution time: 
+		 * long startTime = System.currentTimeMillis(); perform the task; 
+		 * long endTime = System.currentTimeMillis(); 
+		 * long executionTime = endTime - startTime;
+		 */
+
+		ArraySort sortArray = new ArraySort();
+		Integer[] array = new Integer[0];
+		Scanner keyInput = new Scanner(System.in);
+		int userInput;
+		long startTime;
+		long endTime;
+		long executionTime = 0;
+
+		sortArray.sayHello();
+
+		array = sortArray.sizeArray(10000);
+
+		array = sortArray.fillArrayWithRandom(array);
+
+
+		System.out.println("Would you like to sort your numbers in:");
+		System.out.println("1. Ascending Order");
+		System.out.println("2. Descending Order");
+		userInput = keyInput.nextInt();
+
+		if(userInput == 1) {
+			startTime = System.currentTimeMillis();
+			array = sortArray.sortArrayAscending(array);
+			endTime = System.currentTimeMillis();
+			executionTime = endTime - startTime;
+
+		}else if(userInput == 2) {
+			startTime = System.currentTimeMillis();
+			array = sortArray.sortArrayDescending(array);
+			endTime = System.currentTimeMillis();
+			executionTime = endTime - startTime;
+
+		}else {
+			System.out.println("Invalid response.");
+		}
+
+
+		sortArray.printArray(array);
+		System.out.println();
+		System.out.println("It took " + executionTime + " milliseconds for your array to sort.");
+
+
+
+	}
+
+}
