10d6634465f3209921a7eeacd45278e5c32d472b
Sydnie Balcer <sydniebalcer@yahoo.com>
2020-02-28 13:57:18 -0600

    more syd.

---
+++ Sydnie/src/WeekSix/GeometricObject.java
@@ -0,0 +1,63 @@
+package WeekSix;
+
+import java.util.Date;
+
+public class GeometricObject {
+
+	/* -color: String 
+	 * -filled: boolean 
+	 * -dateCreated: java.util.Date
+	 * 
+	 * 
+	 * +GeometricObject()
+	 * Creates a GeometricObject with the specified color and filled  values.
+	 * +GeometricObject(color: String, filled: boolean) 
+	 * +getColor(): String 
+	 * +setColor(color: String): void 
+	 * +isFilled(): boolean 
+	 * +setFilled(filled: boolean): void 
+	 * +getDateCreated(): java.util.Date 
+	 * +toString(): String
+	 * 
+	 */
+
+	private boolean filled;
+	private Date dateCreated; 
+	private String color;
+
+	GeometricObject(){
+		this.setFilled(false);
+		this.dateCreated = new Date();
+		this.setColor("");
+	}
+	
+	GeometricObject(String color, boolean filled){
+		this.setColor(color);
+		this.setFilled(filled);
+		this.dateCreated = new Date();
+	}
+
+	public boolean isFilled() {
+		return filled;
+	}
+
+	public void setFilled(boolean filled) {
+		this.filled = filled;
+	}
+
+	public String getColor() {
+		return color;
+	}
+
+	public void setColor(String color) {
+		this.color = color;
+	}
+
+	public Date getDateCreated() {
+		return dateCreated;
+	}
+	
+
+
+}
+
---
+++ Sydnie/src/WeekSix/Triangle.java
@@ -0,0 +1,88 @@
+package WeekSix;
+
+public class Triangle extends GeometricObject{
+
+	/* -side1: double
+	 * -side2: double
+	 * -side3: double
+	 * 
+	 * 
+	 * +Triangle()
+	 * +Triangle(side1: double, side2: double, side3: double)
+	 * +getSide1() : double
+	 * +setSide1(side1: double) : void 
+	 * +getSide2() : double
+	 * +setSide2(side2: double) : void
+	 * +getSide3() : double
+	 * +setSide3(side3: double) : void
+	 * +getPerimeter() : double
+	 * +getArea() : double 
+	 * +toString() : String
+	 */
+	private double side1;
+	private double side2;
+	private double side3;
+	
+	Triangle(){
+		this.setSide1(1.0);
+		this.setSide2(1.0);
+		this.setSide3(1.0);
+
+	}
+	
+	Triangle(double side1, double side2, double side3){
+		this.side1 = side1;
+		this.side2 = side2;
+		this.side3 = side3;
+		
+	}
+
+	public double getSide1() {
+		return side1;
+	}
+
+	public void setSide1(double side1) {
+		this.side1 = side1;
+	}
+
+	public double getSide2() {
+		return side2;
+	}
+
+	public void setSide2(double side2) {
+		this.side2 = side2;
+	}
+
+	public double getSide3() {
+		return side3;
+	}
+
+	public void setSide3(double side3) {
+		this.side3 = side3;
+	}
+	
+	public double getPerimeter() {
+		double perimeter = side1 + side2 + side3;
+		return perimeter;
+	}
+	
+	//Area=SQRT(s(s-a)(s-b)(s-c))
+	public double getArea() {
+		double s = getPerimeter() / 2;
+		double area = Math.sqrt((s * (s-this.side1) * (s - this.side2) * (s - this.side3)));
+		return area;
+	}
+	
+	public String toString() {
+		String description = super.toString() + "Triangle: side1 = " + this.side1 + " side2 = " + this.side2 + " side3 = " + this.side3;
+		return description;
+	}
+
+
+	
+	
+	
+	
+	
+	
+}
